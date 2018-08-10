c57302bf5ec3867ce922eccadd87e8f687971734
tebell38 <41754328+tebell38@users.noreply.github.com>
2018-08-10 16:17:09 -0400

    commit

--- AzurePowershellStorageAccountsDeployment.ps1
+++ AzurePowershellStorageAccountsDeployment.ps1
@@ -1,3 +1,3 @@
 Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName 'Visual Studio Enterprise'
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName 'storageacctrg20180807'  -TemplateUri https://raw.githubusercontent.com/tebell38/tutorialrepository/master/azuredeploystorageaccount20180808.json -TemplateParameterUri https://raw.githubusercontent.com/tebell38/tutorialrepository/master/storageacctsparametersFile20180808.json  
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName 'storageacctrg20180807'  -TemplateUri https://raw.githubusercontent.com/tebell38/tutorialrepository/master/azuredeploystorageaccount.json -TemplateParameterUri https://raw.githubusercontent.com/tebell38/tutorialrepository/master/storageacctsparameters.json  
